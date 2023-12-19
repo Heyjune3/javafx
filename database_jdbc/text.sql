fc07ea5a3ed6f1d03c6875a455cdc644b54ec9c0
Heyjune3 <kfd45674@gmail.com>
2023-12-19 16:28:17 +0900

    first commit

---
+++ database_jdbc/text.sql
@@ -0,0 +1,3 @@
+
+-- 실행할 쿼리 블럭 지정 후 alt + x or alt + c == Mysql과 같이 실행됨
+SELECT * FROM userTbl;  -- 해당 행 지정 후 alt + s
---
+++ database_practice/src/TestDriver.java
@@ -0,0 +1,22 @@
+import java.sql.Connection;
+
+import utils.DBUtil;
+
+public class TestDriver {
+
+	public static void main(String[] args) {
+		
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			System.out.println("드라이버 등록 완료");
+			Connection conn = DBUtil.getConnection();
+			System.out.println(conn);
+			conn = DBUtil.getConnection();
+			System.out.println(conn);
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		
+	}
+
+}
